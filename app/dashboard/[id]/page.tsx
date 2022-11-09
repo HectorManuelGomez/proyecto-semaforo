59b8201b743538f75df02206cbd75a9604788e39
Hector Gomez <hector.gomez@cumplimos.co>
2022-11-08 20:51:11 -0500

    Implementación imgs semaforo

--- app/dashboard/[id]/page.tsx
+++ app/dashboard/[id]/page.tsx
@@ -1,10 +1,14 @@
 // const fetchData = () => {
 //   return fetch("").then((rest) => rest.json());
 // };
+
 "use client";
 import { Button, Grid } from "@mui/material";
 import { DataGrid, GridColumns, GridCellParams } from "@mui/x-data-grid";
 import Link from "next/link";
+import Image from "next/image";
+import semaforo from "./semaforo.png";
+import { idID } from "@mui/material/locale";
 
 const getTabla = (id) => {
   const listaSemaforos = {
@@ -17,10 +21,18 @@ const getTabla = (id) => {
   return semaforo;
 };
 
+async function getData() {
+  const res = await fetch(
+    "http://127.0.0.1:8090//api/collections/grupo1/records"
+  );
+  const data = await res.json();
+  console.log(data);
+}
+
 export default function Dashboard({ params }) {
   const { id } = params;
-  const SS = 'ss';
-  const TIME = 'time';
+  const SS = "ss";
+  const TIME = "time";
   const data = {
     ss: [
       "01010000",
@@ -51,13 +63,14 @@ export default function Dashboard({ params }) {
   };
   const columns: GridColumns = [
     { headerName: SS, field: SS, width: 80, headerAlign: "center" },
-    { headerName: TIME, field: TIME, width: 80, headerAlign: "center" }
-  ]
+    { headerName: TIME, field: TIME, width: 80, headerAlign: "center" },
+  ];
+  getData();
 
   // const data = await fetchData()
   return (
     <>
-      <h1>Estado semaforo {getTabla(id)}</h1>
+      <h1>Estado semáforo {getTabla(id)}</h1>
       <Grid
         container
         item
@@ -67,12 +80,33 @@ export default function Dashboard({ params }) {
         alignItems="center"
       >
         {/* <DataGrid rows={data} columns={columns} density="compact"/> */}
+
+        {id == 1 && (
+        <Grid item sm={6}>
+          <h2>Grupo 2</h2>
+          <Image src={semaforo} alt="Semaforo" width={400} height={400} />
+          <Image src={semaforo} alt="Semaforo" width={400} height={400} />
+        </Grid>
+        )}
+        <Grid item sm={6}>
+          <h2>Grupo 2</h2>
+          <Image src={semaforo} alt="Semaforo" width={400} height={400} />
+          <Image src={semaforo} alt="Semaforo" width={400} height={400} />
+        </Grid>
+        <Grid item sm={6}>
+          <h2>Grupo 3</h2>
+          <Image src={semaforo} alt="Semaforo" width={400} height={400} />
+        </Grid>
+        <Grid item sm={6}>
+          <h2>Grupo 4</h2>
+          <Image src={semaforo} alt="Semaforo" width={400} height={400} />
+        </Grid>
       </Grid>
+
+      <br></br>
       <Link href="/dashboard">
         <Button variant="contained">Volver</Button>
       </Link>
     </>
   );
 }
-
-
